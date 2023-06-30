cc953f03fb042b07b0e9556d9708dc4fbe23a741
PeterzzCode <pedroadominguez@hotmail.com>
2023-06-29 22:29:52 -0300

    ClienteNegocio Agregado Completo

---
+++ Negocio/ClientesNegocio.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+namespace Negocio
+{
+    public class ClientesNegocio
+    {
+
+        public List<Cliente> listar()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            AccesoDatos datos = new AccesoDatos();
+
+
+            try
+            {
+                datos.setearQuery("Select p.id as Id,p.nombre as Nombre from Clientes as p");
+
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Cliente aux = new Cliente();
+
+
+
+                    if (!(datos.Lector["Id"] is DBNull))
+                        aux.Id = (int)datos.Lector["Id"];
+
+                    if (!(datos.Lector["Nombre"] is DBNull))
+                        aux.Nombre = (string)datos.Lector["Nombre"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+
+        public int TraerUltimoId()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                int cont = 0;
+                datos.setearQuery("select top(1) id from Clientes order by id desc");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    cont = (int)datos.Lector["id"];
+                }
+
+                return cont + 1;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void AgregarCliente(Cliente cliente)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearQuery("Insert into Clientes (id, Nombre)  values (" + cliente.Id + ", '" + cliente.Nombre + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void EliminarCliente(string codigo)
+        {
+
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearQuery("delete from Clientes where id= " + codigo);
+                datos.ejecutarAccion();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public bool ExisteNombreCliente(string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearQuery("select id from Clientes where nombre = '" + nombre + "'");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public string TraerNombreCliente(string id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+
+                
+                datos.setearQuery("select nombre from Clientes where id =" + id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    string nombre =(string) datos.Lector["nombre"];
+                    return nombre;
+                }
+
+                return "";
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+        public void ModificarCliente(string id, string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearQuery("update Clientes set nombre = '" + nombre + "' where id = " + id);
+                datos.ejecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
